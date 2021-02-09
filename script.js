86b5134efdf0c01f38b4243b5b5df2136e13f32f
ChandraWijaya <cw62753@gmail.com>
2021-02-09 10:56:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var alas = prompt('Masukkan nilai Alas');
+var tinggi = prompt('Masukkan nilai Tinggi');
+var luas = (alas * tinggi) / 2;
+console.log('Alas Segitiga  : ' + alas)
+console.log('Tinggi Segitiga ' + tinggi)
+console.log('Luas Segitiga : ' + luas)
